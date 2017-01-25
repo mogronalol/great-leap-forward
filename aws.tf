2a4f259ae9a1853371a89a9918fc31415debcc40
Andrew Morgan <andrew.morgan@opencredo.com>
2017-01-25 16:44:53 +0000

    replaced repeated declarations with counts

--- aws.tf
+++ aws.tf
@@ -7,7 +7,8 @@ provider "aws" {
 resource "aws_vpc" "morgan-vpc" {
   cidr_block = "10.0.0.0/16"
   tags {
-    Name = "terraform-aws-morgan-vpc"
+    owner = "andrew-morgan"
+    name = "etcd-vpc"
   }
 }
 
@@ -15,55 +16,30 @@ resource "aws_subnet" "subnet" {
     count = 3
 
     vpc_id = "${aws_vpc.morgan-vpc.id}"
-    cidr_block = "${cidrsubnet("10.0.0.0/24", 8, count.index)}"
+    cidr_block = "${cidrsubnet("10.0.0.0/16", 8, count.index)}"
     availability_zone = "${element(var.zones, count.index)}"
     map_public_ip_on_launch = true
 
     tags {
       owner = "andrew-morgan"
-      name = "subnet-${count.index}"
+      name = "etcd-subnet-${count.index}"
     }
 }
 
-resource "aws_instance" "etcd-node-a" {
+resource "aws_instance" "instance" {
+  count = 3
+
   ami = "ami-1967056a"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.subnet.0.id}"
-  private_ip = "10.0.0.4"
+  subnet_id = "${element(aws_subnet.subnet.*.id, count.index)}"
+  private_ip = "${cidrhost("${element(aws_subnet.subnet.*.cidr_block, count.index)}", 4)}"
   depends_on = ["aws_internet_gateway.gw"]
   security_groups = ["${aws_security_group.sg.id}"]
   key_name = "${aws_key_pair.deployer.id}"
 
   tags {
-    Name = "morgan-etc-instance-a"
-  }
-}
-
-resource "aws_instance" "etcd-node-b" {
-  ami = "ami-1967056a"
-  instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.subnet.1.id}"
-  private_ip = "10.0.1.4"
-  depends_on = ["aws_internet_gateway.gw"]
-  security_groups = ["${aws_security_group.sg.id}"]
-  key_name = "${aws_key_pair.deployer.id}"
-
-  tags {
-    Name = "morgan-etc-instance-b"
-  }
-}
-
-resource "aws_instance" "etcd-node-c" {
-  ami = "ami-1967056a"
-  instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.subnet.2.id}"
-  private_ip = "10.0.2.4"
-  depends_on = ["aws_internet_gateway.gw"]
-  security_groups = ["${aws_security_group.sg.id}"]
-  key_name = "${aws_key_pair.deployer.id}"
-
-  tags {
-    Name = "morgan-etc-instance-c"
+    owner = "andrew-morgan"
+    name = "etcd-node-for-subnet-${count.index}"
   }
 }
 
@@ -72,29 +48,20 @@ resource "aws_key_pair" "deployer" {
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCv97yQNdOSH+XPc9OdpdZPYVDH58c2HjwLv3KDStyuEk47T/E05gm7b0jeUwNQfnfRRJGH7LFK2nl6Km8Da/08Vlev+BG8O42M5gM6oLAeJY1zvAC+3fQ9kJAThyCj4Paz7uFpezy3GdPOx13gAqhlzLI8iDioz+lVQIXSxFS+INuw54d2L/DbPIXglQMYZ/4sQpP0q9Vn7gYX2Yg1PX3xUrd9bRp3P60d3YLgoooblMh6JByqjiWi090oUxhx8YnpU5VWjtYKe/r+skWbKOEELE3O1+HwlAaZNDtFJZYapov9Z/wZ4JTtV+PJrR9ZCDA/HWocZaQT/CGmqHw3zlKz andrewmorgan1@Andrews-MacBook-Pro.local"
 }
 
-resource "aws_eip" "a" {
-  vpc = true
-  instance = "${aws_instance.etcd-node-a.id}"
-  associate_with_private_ip = "10.0.0.4"
-}
+resource "aws_eip" "eip" {
+  count = 3
 
-resource "aws_eip" "b" {
   vpc = true
-  instance = "${aws_instance.etcd-node-b.id}"
-  associate_with_private_ip = "10.0.1.4"
-}
-
-resource "aws_eip" "c" {
-  vpc = true
-  instance = "${aws_instance.etcd-node-c.id}"
-  associate_with_private_ip = "10.0.2.4"
+  instance = "${element(aws_instance.instance.*.id, count.index)}"
+  associate_with_private_ip = "${element(aws_instance.instance.*.private_ip, count.index)}"
 }
 
 resource "aws_internet_gateway" "gw" {
     vpc_id = "${aws_vpc.morgan-vpc.id}"
 
     tags {
-      Name = "morgan-igw"
+      owner = "andrew-morgan"
+      name = "etdc-internet-gateway"
     }
 }
 
@@ -110,20 +77,12 @@ resource "aws_route_table" "rt" {
   }
 }
 
-resource "aws_route_table_association" "rt_association_a" {
-    subnet_id = "${aws_subnet.subnet.0.id}"
+resource "aws_route_table_association" "rt_association" {
+    count = 3
+
+    subnet_id = "${element(aws_subnet.subnet.*.id, count.index)}"
     route_table_id = "${aws_route_table.rt.id}"
 }
-
-resource "aws_route_table_association" "rt_association_b" {
-    subnet_id = "${aws_subnet.subnet.1.id}"
-    route_table_id = "${aws_route_table.rt.id}"
-}
-
-/*resource "aws_route_table_association" "rt_association_c" {
-    subnet_id = "${aws_subnet.subnet.2.id}"
-    route_table_id = "${aws_route_table.rt.id}"
-}*/
 
 resource "aws_security_group" "sg" {
     name = "morgan-security-group"
